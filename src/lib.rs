d96b6ebc3e30221a189941e36d8fe0142b0c6c08
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-12-08 23:44:26 -0500

    Create lib.rs
    from upstream https://github.com/Argyle-Software/dilithium

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(feature = "aes")]
+mod aes256ctr;
+mod api;
+mod fips202;
+mod ntt;
+mod packing;
+mod params;
+mod poly;
+mod polyvec;
+mod randombytes;
+mod reduce;
+mod rounding;
+mod sign;
+mod symmetric;
+pub use params::*;
+
+pub use api::*;
+
+#[cfg(feature = "wasm")]
+mod wasm;
+
+#[cfg(dilithium_kat)]
+pub use sign::{
+  crypto_sign_keypair, crypto_sign_signature, crypto_sign_verify,
+};
