e481c48bbe606e5b250a9fa193aa58fc73b07bc4
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-07-17 22:41:19 -0400

    Update randombytes.rs
    fix typo

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -2,5 +2,5 @@ use rand::TryRngCore;
 use rand::rngs::OsRng;
 
 pub fn randombytes(x: &mut [u8], len: usize) {
-  OsRng.try_file_bytes(&mut x[..len]).expect("OS failed to provide entropy")
+  OsRng.try_fill_bytes(&mut x[..len]).expect("OS failed to provide entropy")
 }
