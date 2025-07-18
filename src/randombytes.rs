8009e45d45904bd1014ae59f3d0abd68d068b0b3
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-07-17 22:39:59 -0400

    Update randombytes.rs
    rand crate version migration and improvement

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -1,5 +1,6 @@
-use rand::prelude::*;
+use rand::TryRngCore;
+use rand::rngs::OsRng;
 
 pub fn randombytes(x: &mut [u8], len: usize) {
-  thread_rng().fill_bytes(&mut x[..len])
+  OsRng.try_file_bytes(&mut x[..len]).expect("OS failed to provide entropy")
 }
