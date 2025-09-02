e68421e8aae2a3b5fc7060ffbdfd26dd32eddd28
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-01 20:10:30 -0400

    Update randombytes.rs
    add doc comment for randombytes

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -1,6 +1,7 @@
 use rand::TryRngCore;
 use rand::rngs::OsRng;
 
+/// Generate random bytes from OS RNG.
 pub fn randombytes(x: &mut [u8], len: usize) {
   OsRng.try_fill_bytes(&mut x[..len]).expect("OS failed to provide entropy")
 }
