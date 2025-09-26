e754765b59b24d685ddc005622b2d2af5bb9983e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-26 16:14:14 -0400

    Update main.rs
    version printing update to 0.1.10

--- src/main.rs
+++ src/main.rs
@@ -510,7 +510,7 @@ fn help() -> Result<(), Box<dyn StdError>> {
 
 /// Print the version.
 fn version() -> Result<(), Box<dyn StdError>> {
-    println!("{{\"Version\": \"0.1.9\"}}");
+    println!("{{\"Version\": \"0.1.10\"}}");
     Ok(())
 }
 
